bcc9de53c70a1940c6ceab6b5cad36e5f1ac7adb
Sylvain Pruvot <84276622+Sylvpruv@users.noreply.github.com>
2023-01-17 11:06:23 +0100

    Update waiting_times_surgery2.py

--- waiting_times_surgery2.py
+++ waiting_times_surgery2.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 from plotly.subplots import make_subplots
-import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 
 #Reading of the dataset
